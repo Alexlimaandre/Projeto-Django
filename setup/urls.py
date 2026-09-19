371b415c9600751ba51cd644ff386e7744ec3aa9
Alexlimaandre <alexandre.limaandre@gmail.com>
2026-09-19 14:16:26 -0300

    [FEAT] Atualização, deleção e conclsão de tarefas implementados

--- setup/urls.py
+++ setup/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.TodoHomeView.as_view(), name="home"),
     path("lista/", views.TodoListView.as_view(), name="todo_list"),
-    path("criar/", views.TodoCreateView.as_view(), name="todo_create")
+    path("criar/", views.TodoCreateView.as_view(), name="todo_create"),
+    path("atualizar/<int:pk>/", views.TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>/", views.TodoDeleteView.as_view(), name="todo_delete"),
+    path("concluir/<int:pk>/", views.TodoCompleteView.as_view(), name="todo_complete")
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
 from .models import Todo
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404
+from datetime import datetime
 
 # def todo_list(req):
 #     nome = "Alex"
@@ -23,3 +25,25 @@ class TodoCreateView(CreateView):
     template_name = "todos/todo_create.html"
     fields = ["title","deadline"] # Usando o CreateView, é necessário informar os fields que serão exibidos no formulário, ou seja, os campos que o usuário poderá preencher. Caso não seja informado, o Django exibirá todos os campos do modelo.
     success_url = reverse_lazy("todo_list") # Após o usuário preencher o formulário, ele será redirecionado para a página de listagem de tarefas. O reverse_lazy é usado para gerar a URL da view "todo_list" de forma dinâmica, sem precisar hardcodar a URL.
+    
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ["title","deadline"]
+    success_url = reverse_lazy("todo_list")
+    template_name = "todos/todo_create.html"
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = "todos/todo_delete.html"
+    success_url = reverse_lazy("todo_list")
+    
+class TodoCompleteView(View):
+    def get(self, request, pk):
+        # modulo para buscar o objeto do banco de dados, caso não exista, retorna um erro 404
+        todo =get_object_or_404(Todo, pk=pk)
+        # marcar a tarefa como concluída, basta alterar o campo finished_at para a data atual e salvar o objeto no banco de dados
+        todo.finished_at = datetime.now()
+        # salvar o objeto no banco de dados
+        todo.save()
+        # redirecionar o usuário para a página de listagem de tarefas
+        return redirect("todo_list")
